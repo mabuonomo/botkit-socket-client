cd2eb3839f49cf9c4bb30b8a6705375694ed0c1b
Mario Buonomo <ma.buonomo@gmail.com>
2020-01-19 00:21:35 +0100

    improve

--- build/index.js
+++ build/index.js
@@ -1,19 +1,89 @@
 "use strict";
-var __importDefault =
-  (this && this.__importDefault) ||
-  function(mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const socket_io_client_1 = __importDefault(require("socket.io-client"));
 console.log("start");
-const socket = socket_io_client_1.default("http://localhost:3000", {
-  transports: ["websocket"]
+const socket = socket_io_client_1.default("ws://localhost:3000/", {
+    transports: ["websocket"]
+});
+function generate_guid() {
+    function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000)
+            .toString(16)
+            .substring(1);
+    }
+    return (s4() +
+        s4() +
+        "-" +
+        s4() +
+        "-" +
+        s4() +
+        "-" +
+        s4() +
+        "-" +
+        s4() +
+        s4() +
+        s4());
+}
+let connectEvent = "hello";
+let guid = generate_guid();
+let channel = "websocket";
+console.log(connectEvent, guid, channel);
+socket.on("connection", socket => {
+    console.log("connection");
+});
+socket.on("connected", function () {
+    console.log("CONNECTED TO SOCKET");
+});
+socket.on("disconnected", function () {
+    console.log("CONNECTED TO SOCKET");
+});
+socket.on("webhook_error", function (err) {
+    alert("Error sending message!");
+    console.error("Webhook Error", err);
 });
 console.log("send hello");
-socket.emit("message", { type: "hello", message: "" });
-socket.on("news", function(data) {
-  console.log(data);
-  socket.emit("my other event", { my: "data" });
+socket.send(JSON.stringify({
+    type: connectEvent,
+    user: guid,
+    channel: channel,
+    user_profile: null
+}));
+socket.send(JSON.stringify({
+    type: "message",
+    text: "ciao",
+    user: channel,
+    channel: "websocket"
+}));
+socket.addEventListener("open", function (event) {
+    console.log("CONNECTED TO SOCKET");
+    socket.send(JSON.stringify({
+        type: connectEvent,
+        user: guid,
+        channel: "websocket",
+        user_profile: null
+    }));
 });
-exports.default = socket_io_client_1.default;
+socket.addEventListener("error", function (event) {
+    console.error("ERROR", event);
+});
+socket.addEventListener("close", function (event) {
+    console.log("SOCKET CLOSED!");
+});
+socket.addEventListener("message", function (event) {
+    var message = null;
+    try {
+        message = JSON.parse(event.data);
+        console.log(message);
+    }
+    catch (err) {
+        console.log(err);
+        return;
+    }
+});
+console.log("here");
+socket.on("message", function (message) {
+    console.log("RECEIVED MESSAGE", message);
+});
--- index.ts
+++ index.ts
@@ -1,20 +1,132 @@
 import io from "socket.io-client";
 
 console.log("start");
-const socket = io("http://localhost:3000", {
+const socket = io("ws://localhost:3000/", {
   transports: ["websocket"]
 });
 
-// import { ChatServer } from "./chat-server";
-// let app = new ChatServer().getApp();
-// export { app };
+function generate_guid() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return (
+    s4() +
+    s4() +
+    "-" +
+    s4() +
+    "-" +
+    s4() +
+    "-" +
+    s4() +
+    "-" +
+    s4() +
+    s4() +
+    s4()
+  );
+}
 
-console.log("send hello");
-socket.emit("message", { type: "hello", message: "" });
+let connectEvent = "hello";
+let guid = generate_guid();
+let channel = "websocket";
 
-socket.on("news", function(data) {
-  console.log(data);
-  socket.emit("my other event", { my: "data" });
+console.log(connectEvent, guid, channel);
+
+socket.on("connection", socket => {
+  console.log("connection");
 });
 
-export default io;
+socket.on("connected", function() {
+  console.log("CONNECTED TO SOCKET");
+});
+
+socket.on("disconnected", function() {
+  console.log("CONNECTED TO SOCKET");
+});
+
+socket.on("webhook_error", function(err) {
+  alert("Error sending message!");
+  console.error("Webhook Error", err);
+});
+
+// message = {
+//   type: "outgoing",
+//   text: text
+// };
+
+console.log("send hello");
+// socket.emit("hello", { type: "hello", text: "hello" });
+// socket.emit("message", { type: "hello", text: "hello" });
+
+socket.send(
+  JSON.stringify({
+    type: connectEvent,
+    user: guid,
+    channel: channel,
+    user_profile: null
+  })
+);
+
+socket.send(
+  JSON.stringify({
+    type: "message",
+    text: "ciao",
+    user: channel,
+    channel: "websocket"
+  })
+);
+
+socket.addEventListener("open", function(event) {
+  console.log("CONNECTED TO SOCKET");
+  // socket.reconnect_count = 0;
+  // socket.trigger("connected", event);
+  socket.send(
+    JSON.stringify({
+      type: connectEvent,
+      user: guid,
+      channel: "websocket", //"socket",
+      user_profile: null
+    })
+  );
+});
+
+socket.addEventListener("error", function(event) {
+  console.error("ERROR", event);
+});
+
+socket.addEventListener("close", function(event) {
+  console.log("SOCKET CLOSED!");
+  // socket.trigger("disconnected", event);
+  // if (socket.reconnect_count < socket.config.max_reconnect) {
+  //   setTimeout(function() {
+  //     console.log("RECONNECTING ATTEMPT ", ++socket.reconnect_count);
+  //     socket.connectWebsocket(socket.config.ws_url);
+  //   }, socket.config.reconnect_timeout);
+  // } else {
+  //   socket.message_window.className = "offline";
+  // }
+});
+
+// Listen for messages
+socket.addEventListener("message", function(event) {
+  var message = null;
+  try {
+    message = JSON.parse(event.data);
+    console.log(message);
+  } catch (err) {
+    console.log(err);
+    // socket.trigger("socket_error", err);
+    return;
+  }
+});
+
+console.log("here");
+socket.on("message", function(message) {
+  console.log("RECEIVED MESSAGE", message);
+});
+
+// socket.on("message", function (data) {
+//   console.log(data);
+//   socket.emit("my other event", { my: "data" });
+// });
